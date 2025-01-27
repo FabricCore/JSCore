679bb593ecbd45377cf6cdbef9f1853910824b78
siriusmart <siriusmart@tuta.io>
2025-01-27 23:28:09 +0000

    added nonblocking run mod for runnable

--- src/main/java/ws/siri/jscore/Core.java
+++ src/main/java/ws/siri/jscore/Core.java
@@ -1,28 +1,16 @@
 package ws.siri.jscore;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.HashMap;
 import java.util.Optional;
 
-import org.apache.commons.lang3.StringEscapeUtils;
 import org.jetbrains.annotations.Nullable;
 import org.mozilla.javascript.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.mojang.brigadier.arguments.StringArgumentType;
-
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
-import net.fabricmc.fabric.api.event.Event;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import ws.siri.jscore.wraps.Runnable;
@@ -31,9 +19,9 @@ public class Core implements ModInitializer {
     public static final String MOD_ID = "jscore";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    private static yarnwrap.client.MinecraftClient client;
-    private static Context rhino;
-    private static Scriptable rhinoScope;
+    public static yarnwrap.client.MinecraftClient client;
+    public static Context rhino;
+    public static Scriptable rhinoScope;
 
     public static yarnwrap.client.MinecraftClient getClient() {
         return client;
@@ -50,36 +38,6 @@ public class Core implements ModInitializer {
 
         Loader.init();
 
-        ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-            dispatcher.register(ClientCommandManager.literal("jseval")
-                    .then(ClientCommandManager.argument("code", StringArgumentType.greedyString())
-                            .executes(context -> {
-                                String code = StringArgumentType.getString(context, "code");
-                                log(code, Formatting.GREEN);
-                                Optional<Object> res = evalTyped(code);
-                                if(res.isEmpty()) return 1;
-                                String text = res.get().toString();
-
-                                if(text.equals("undefined")) {
-                                    log("undefined", Formatting.GRAY);
-                                } else {
-                                    Formatting color = Formatting.GRAY;
-                                    if(!text.isBlank()) {
-                                        color = Formatting.YELLOW;
-                                    } else {
-                                        text = "[empty]";
-                                    }
-
-                                    log(text, color);
-                                }
-                                return 1;
-                            })));
-        });
-
-        // java.util.function.Function f = (mc) -> {};
-
-        // ClientTickEvents.END_CLIENT_TICK.register(f);
-
         ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((client, world) -> {
             playerOutdated = true;
         });
@@ -93,6 +51,9 @@ public class Core implements ModInitializer {
                 playerOutdated = false;
             }
         });
+
+        // BodyHandler
+        // HttpClient
 
         LOGGER.info("JSCore powered up.");
     }
@@ -148,7 +109,11 @@ public class Core implements ModInitializer {
         return Optional.of(Context.jsToJava(res.get(), Object.class));
     }
 
-    public static Runnable runnable(String source) {
-        return new Runnable(source);
+    public static Runnable runnable(String ident, String function) {
+        return Runnable.create(ident, function);
+    }
+
+    public static Runnable getRunnable(String ident) {
+        return Runnable.runnables.get(ident);
     }
 }
--- src/main/java/ws/siri/jscore/wraps/Runnable.java
+++ src/main/java/ws/siri/jscore/wraps/Runnable.java
@@ -1,5 +1,17 @@
 package ws.siri.jscore.wraps;
 
+import java.util.HashMap;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Function;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents.ClientStarted;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents.ClientStopping;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.EndTick;
@@ -8,56 +20,94 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.StartT
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents.StartWorldTick;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.server.world.ServerWorld;
 import ws.siri.jscore.Core;
 
 public class Runnable implements
-    ClientStarted, ClientStopping, // ClientLifecycleEvents.java
-    StartTick, EndTick, StartWorldTick, EndWorldTick // ClientTickEvents.java
-     {
-    String source;
-    String code;
+        ClientStarted, ClientStopping, // ClientLifecycleEvents.java
+        StartTick, EndTick, StartWorldTick, EndWorldTick, // ClientTickEvents.java
 
-    public Runnable(String source) {
-        this.source = source;
-        this.code = String.format("%s();", source);
+        ClientCommandRegistrationCallback, Command<FabricClientCommandSource>, // brigadier
+        java.lang.Runnable
+{
+    public static HashMap<String, Runnable> runnables = new HashMap<>();
+    private Function f;
+    private Object[] spawnArgs;
+
+    private Runnable(String ident, String function) {
+        f = Core.rhino.compileFunction(Core.rhinoScope, function, ident, 1, null);
+        runnables.put(ident, this);
     }
 
-    public static Runnable create(String source) {
-        return new Runnable(source);
+    public static Runnable create(String ident, String function) {
+        if (runnables.containsKey(ident)) {
+            Function f = Core.rhino.compileFunction(Core.rhinoScope, function, ident, 1, null);
+            runnables.get(ident).f = f;
+            return runnables.get(ident);
+        } else {
+            return new Runnable(ident, function);
+        }
     }
 
-    public void run() {
-        Core.eval(code);
+    public void runF(Object... args) {
+        f.call(Core.rhino, Core.rhinoScope, null, args);
+    }
+
+    public void runFCtx(Context ctx, Object... args) {
+        f.call(ctx, Core.rhinoScope, null, args);
     }
 
     @Override
     public void onClientStarted(MinecraftClient client) {
-        run();
+        runF(new yarnwrap.client.MinecraftClient(client));
     }
 
     @Override
     public void onClientStopping(MinecraftClient client) {
-        run();
+        runF(new yarnwrap.client.MinecraftClient(client));
     }
 
     @Override
     public void onStartTick(MinecraftClient client) {
-        run();
+        runF(new yarnwrap.client.MinecraftClient(client));
     }
 
     @Override
     public void onEndTick(MinecraftClient client) {
-        run();
+        runF(new yarnwrap.client.MinecraftClient(client));
     }
 
     @Override
     public void onStartTick(ServerWorld world) {
-        run();
+        runF(new yarnwrap.server.world.ServerWorld(world));
     }
 
     @Override
     public void onEndTick(ClientWorld world) {
-        run();
+        runF(new yarnwrap.client.world.ClientWorld(world));
+    }
+
+    @Override
+    public int run(CommandContext<FabricClientCommandSource> context) throws CommandSyntaxException {
+        runF(context);
+        return 1;
+    }
+
+    @Override
+    public void register(CommandDispatcher<FabricClientCommandSource> dispatcher,
+            CommandRegistryAccess registryAccess) {
+        runF(dispatcher, registryAccess);
+    }
+
+    @Override
+    public void run() {
+        Context ctx = Context.enter();
+        runFCtx(ctx, spawnArgs);
+    }
+
+    public void spawn(Object... spawnArgs) {
+        this.spawnArgs = spawnArgs;
+        new Thread(this).start();
     }
 }
