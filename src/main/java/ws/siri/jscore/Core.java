f8a78f09e9ea23d9d51d677dd70baf23d190abd5
siriusmart <siriusmart@tuta.io>
2025-04-29 20:24:59 +0100

    jscore no longer dependent on yarnwrap #2

--- src/main/java/ws/siri/jscore/Core.java
+++ src/main/java/ws/siri/jscore/Core.java
@@ -8,8 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientWorldEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -19,63 +17,29 @@ public class Core implements ModInitializer {
     public static final String MOD_ID = "jscore";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    public static yarnwrap.client.MinecraftClient client;
     public static Context rhino;
     public static Scriptable rhinoScope;
 
     public static CmdRunnableCore runnable = new CmdRunnableCore();
 
-    public static yarnwrap.client.MinecraftClient getClient() {
-        return client;
-    }
-
-    private boolean playerOutdated;
-    private boolean playerIsSet = false;
-
     @Override
     public void onInitialize() {
-        client = new yarnwrap.client.MinecraftClient(MinecraftClient.getInstance());
-
         rhino = Context.enter();
         rhinoScope = rhino.initStandardObjects();
 
         Loader.init();
-        
-
-        ClientWorldEvents.AFTER_CLIENT_WORLD_CHANGE.register((client, world) -> {
-            playerOutdated = true;
-        });
-
-        ClientTickEvents.START_CLIENT_TICK.register((mc) -> {
-            if (mc.world == null && playerIsSet) {
-                eval("clearPlayer();");
-                playerIsSet = false;
-                return;
-            }
-
-            if (playerOutdated) {
-                eval("updatePlayer();");
-                playerOutdated = false;
-                playerIsSet = true;
-            }
-        });
 
         LOGGER.info("JSCore powered up.");
     }
 
-    public static void log(String s, Formatting formatting) {
-        if(client.wrapperContained.inGameHud == null) LOGGER.info(s);
-        else client.wrapperContained.inGameHud.getChatHud().addMessage(Text.literal(s).formatted(formatting));
-    }
-
     public static void log(String s) {
-        if(client.wrapperContained.inGameHud == null) LOGGER.info(s);
-        else client.wrapperContained.inGameHud.getChatHud().addMessage(Text.literal(s).formatted(Formatting.GRAY));
+        if(MinecraftClient.getInstance().inGameHud == null) LOGGER.info(s);
+        else MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.literal(s).formatted(Formatting.GRAY));
     }
 
     public static void error(String s) {
-        if(client.wrapperContained.inGameHud == null) LOGGER.info(s);
-        else client.wrapperContained.inGameHud.getChatHud().addMessage(Text.literal(s).formatted(Formatting.RED));
+        if(MinecraftClient.getInstance().inGameHud == null) LOGGER.info(s);
+        else MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.literal(s).formatted(Formatting.RED));
     }
 
     public static Object evalUncatched(String statement, String file) throws Exception {
